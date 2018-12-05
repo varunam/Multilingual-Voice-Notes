acafb2bd8177614113767b1b71828c2fc3ed0e78
Varun A M <varun.am@olacabs.com>
2018-12-05 20:44:14 +0530

    added code to handle click with animation

---
+++ app/src/main/java/com/kannadavoicenotes/app/kannadavoicenotes/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.kannadavoicenotes.app.kannadavoicenotes
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Created by varun.am on 05/12/18
+ */
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
--- app/src/main/java/com/kannadavoicenotes/app/kannadavoicenotes/view/fragments/SpeechToTextFragment.kt
+++ app/src/main/java/com/kannadavoicenotes/app/kannadavoicenotes/view/fragments/SpeechToTextFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 import com.kannadavoicenotes.app.kannadavoicenotes.R
 
 /**
  * Created by varun.am on 05/12/18
  */
-class SpeechToTextFragment : Fragment(){
+class SpeechToTextFragment : Fragment() {
 
     companion object {
         fun newInstance(): SpeechToTextFragment {
@@ -27,6 +29,16 @@ class SpeechToTextFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val micLayout = view.findViewById<ImageView>(R.id.micId)
+        micLayout.setOnClickListener {
+            startAnimation(view)
+        }
+    }
+
+    private fun startAnimation(view: View) {
+        val lottieAnimation = view.findViewById<LottieAnimationView>(R.id.mic_animation_id)
+        lottieAnimation.playAnimation()
     }
 
 }
