b9930368e248c84dcd9ba17132a9c3f671ebf213
Varun A M <varun.am@olacabs.com>
2018-12-05 19:43:22 +0530

    migrated to androidx

--- app/src/main/java/com/kannadavoicenotes/app/kannadavoicenotes/adapter/ViewpagerAdapter.kt
+++ app/src/main/java/com/kannadavoicenotes/app/kannadavoicenotes/adapter/ViewpagerAdapter.kt
@@ -4,9 +4,9 @@ package com.kannadavoicenotes.app.kannadavoicenotes.adapter
  * Created by varun.am on 05/12/18
  */
 
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.kannadavoicenotes.app.kannadavoicenotes.view.fragments.ChooseLanguageFragment
 
 class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
